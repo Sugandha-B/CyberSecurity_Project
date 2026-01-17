6fab6ff6ced2a972d376a63888fe18a9f556425d
Sugandha <bewithsugandha@gmail.com>
2026-01-18 00:54:15 +0530

    modified main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
-import os    #to work with files,folders and directories
+import os
+from src.utils.loader import load_signatures
+from src.engine.scanner import scan_directory
 
-def scan_files(directory):  #directory is the folder to scan
-    print(f"Scanning directory: {directory}\n")
+def main():
+    # Project root (CYBERSECURITY/)
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    for folder, subfolders, files in os.walk(directory):  #recursively goes through every folder and sub-folder
-        for file in files:
-            file_path = os.path.join(folder, file)  #create and joins the folder path
-            print(file_path)
+    # Folder to scan
+    SAMPLES_DIR = os.path.join(BASE_DIR, "samples")
 
-# paths relative to this file
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))   #points to the project root
-SAMPLES_DIR = os.path.join(BASE_DIR, "samples")   #appends to the base project directory
-#this works regardless from where the script is run
+    print("[*] Loading signatures...")
+    signatures = load_signatures()
 
-print("Resolved samples path:", SAMPLES_DIR)  #confirms the exact directory it will scan
+    print("[*] Starting scan...\n")
+    scan_directory(SAMPLES_DIR, signatures)
 
-scan_files(SAMPLES_DIR)  #calls the function
+    print("\n[*] Scan complete.")
+
+if __name__ == "__main__":
+    main()
